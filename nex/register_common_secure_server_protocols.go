7d75d91a51cdcbc7f53b632cdb6806484e677ae5
shoginyan <shoginyan@gmail.com>
2025-04-14 09:37:04 -0700

    Initial commit

---
+++ nex/register_common_secure_server_protocols.go
@@ -0,0 +1,120 @@
+package nex
+
+import (
+	//"fmt"
+
+	"github.com/PretendoNetwork/nex-go/v2/types"
+	commonnattraversal "github.com/PretendoNetwork/nex-protocols-common-go/v2/nat-traversal"
+	commonsecure "github.com/PretendoNetwork/nex-protocols-common-go/v2/secure-connection"
+	nattraversal "github.com/PretendoNetwork/nex-protocols-go/v2/nat-traversal"
+	secure "github.com/PretendoNetwork/nex-protocols-go/v2/secure-connection"
+	"github.com/PretendoNetwork/yo-kai-watch-2/globals"
+
+	commonmatchmaking "github.com/PretendoNetwork/nex-protocols-common-go/v2/match-making"
+	commonmatchmakingext "github.com/PretendoNetwork/nex-protocols-common-go/v2/match-making-ext"
+	commonmatchmakeextension "github.com/PretendoNetwork/nex-protocols-common-go/v2/matchmake-extension"
+	matchmaking "github.com/PretendoNetwork/nex-protocols-go/v2/match-making"
+	matchmakingext "github.com/PretendoNetwork/nex-protocols-go/v2/match-making-ext"
+	matchmakeextension "github.com/PretendoNetwork/nex-protocols-go/v2/matchmake-extension"
+
+	"strconv"
+	"strings"
+
+	matchmakingtypes "github.com/PretendoNetwork/nex-protocols-go/v2/match-making/types"
+	messagedelivery "github.com/PretendoNetwork/nex-protocols-go/v2/message-delivery"
+	ranking "github.com/PretendoNetwork/nex-protocols-go/v2/ranking"
+
+	//local_matchmakeextension "github.com/PretendoNetwork/yo-kai-watch-2/nex/matchmake-extension"
+	local_match_making "github.com/PretendoNetwork/yo-kai-watch-2/nex/match_making"
+	local_message_delivery "github.com/PretendoNetwork/yo-kai-watch-2/nex/message_delivery"
+)
+
+// Is this needed? -Ash
+func cleanupSearchMatchmakeSessionHandler(matchmakeSession *matchmakingtypes.MatchmakeSession) {
+	matchmakeSession.Attributes[2] = types.NewUInt32(0)
+	matchmakeSession.MatchmakeParam = matchmakingtypes.NewMatchmakeParam()
+	matchmakeSession.ApplicationBuffer = types.NewBuffer(make([]byte, 0))
+	matchmakeSession.GameMode = types.NewUInt32(33)
+	globals.Logger.Info(matchmakeSession.String())
+}
+
+func CreateReportDBRecord(_ types.PID, _ types.UInt32, _ types.QBuffer) error {
+	return nil
+}
+
+// from nex-protocols-common-go/matchmaking_utils.go
+func compareSearchCriteria[T ~uint16 | ~uint32](original T, search string) bool {
+	if search == "" { // * Accept any value
+		return true
+	}
+
+	before, after, found := strings.Cut(search, ",")
+	if found {
+		min, err := strconv.ParseUint(before, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		max, err := strconv.ParseUint(after, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		return min <= uint64(original) && max >= uint64(original)
+	} else {
+		searchNum, err := strconv.ParseUint(before, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		return searchNum == uint64(original)
+	}
+}
+
+func registerCommonSecureServerProtocols() {
+	secureProtocol := secure.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(secureProtocol)
+	commonSecureProtocol := commonsecure.NewCommonProtocol(secureProtocol)
+
+	globals.MatchmakingManager.GetUserFriendPIDs = globals.GetUserFriendPIDs
+
+	commonSecureProtocol.CreateReportDBRecord = CreateReportDBRecord
+
+	natTraversalProtocol := nattraversal.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(natTraversalProtocol)
+	commonnattraversal.NewCommonProtocol(natTraversalProtocol)
+
+	matchMakingProtocol := matchmaking.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(matchMakingProtocol)
+	matchMakingProtocol.FindByOwner = local_match_making.FindByOwner
+	commonmatchmaking.NewCommonProtocol(matchMakingProtocol).SetManager(globals.MatchmakingManager)
+
+	matchMakingExtProtocol := matchmakingext.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(matchMakingExtProtocol)
+	commonmatchmakingext.NewCommonProtocol(matchMakingExtProtocol).SetManager(globals.MatchmakingManager)
+
+	rankingProtocol := ranking.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(rankingProtocol)
+
+	matchmakeExtensionProtocol := matchmakeextension.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(matchmakeExtensionProtocol)
+	commonMatchmakeExtensionProtocol := commonmatchmakeextension.NewCommonProtocol(matchmakeExtensionProtocol)
+	commonMatchmakeExtensionProtocol.SetManager(globals.MatchmakingManager)
+	//matchmakeExtensionProtocol.SetHandlerGetFriendNotificationData(getFriendNotificationData)
+	//matchmakeExtensionProtocol.SetHandlerUpdateNotificationData(updateNotificationData)
+	//matchmakeExtensionProtocol.GetPlayingSession = local_matchmakeextension.GetPlayingSession
+
+	messageDeliveryProtocol := messagedelivery.NewProtocol()
+	globals.SecureEndpoint.RegisterServiceProtocol(messageDeliveryProtocol)
+
+	messageDeliveryProtocol.DeliverMessage = local_message_delivery.DeliverMessage
+
+	commonMatchmakeExtensionProtocol.CleanupSearchMatchmakeSession = cleanupSearchMatchmakeSessionHandler
+	commonMatchmakeExtensionProtocol.CleanupMatchmakeSessionSearchCriterias = func(searchCriterias types.List[matchmakingtypes.MatchmakeSessionSearchCriteria]) {
+		for _, searchCriteria := range searchCriterias {
+			searchCriteria.Attribs[2] = types.NewString("")
+		}
+
+	}
+
+}
