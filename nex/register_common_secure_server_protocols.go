fc1bcdf9460c98e41259f29b0d002410360a35b4
TraceEntertains <atw3710@gmail.com>
2026-07-30 01:20:50 -0400

    chore: remove leftovers from minecraft-wiiu, general cleanup for register_common_secure_server_protocols

--- nex/register_common_secure_server_protocols.go
+++ nex/register_common_secure_server_protocols.go
@@ -29,46 +29,8 @@ import (
 	local_message_delivery "github.com/PretendoNetwork/yo-kai-watch-2/nex/message_delivery"
 )
 
-// Is this needed? -Ash
-func cleanupSearchMatchmakeSessionHandler(matchmakeSession *matchmakingtypes.MatchmakeSession) {
-	matchmakeSession.Attributes[2] = types.NewUInt32(0)
-	matchmakeSession.MatchmakeParam = matchmakingtypes.NewMatchmakeParam()
-	matchmakeSession.ApplicationBuffer = types.NewBuffer(make([]byte, 0))
-	matchmakeSession.GameMode = types.NewUInt32(33)
-	globals.Logger.Info(matchmakeSession.String())
-}
-
 func CreateReportDBRecord(_ types.PID, _ types.UInt32, _ types.QBuffer) error {
 	return nil
-}
-
-// from nex-protocols-common-go/matchmaking_utils.go
-func compareSearchCriteria[T ~uint16 | ~uint32](original T, search string) bool {
-	if search == "" { // * Accept any value
-		return true
-	}
-
-	before, after, found := strings.Cut(search, ",")
-	if found {
-		min, err := strconv.ParseUint(before, 10, 64)
-		if err != nil {
-			return false
-		}
-
-		max, err := strconv.ParseUint(after, 10, 64)
-		if err != nil {
-			return false
-		}
-
-		return min <= uint64(original) && max >= uint64(original)
-	} else {
-		searchNum, err := strconv.ParseUint(before, 10, 64)
-		if err != nil {
-			return false
-		}
-
-		return searchNum == uint64(original)
-	}
 }
 
 func registerCommonSecureServerProtocols() {
@@ -101,16 +63,12 @@ func registerCommonSecureServerProtocols() {
 	globals.SecureEndpoint.RegisterServiceProtocol(matchmakeExtensionProtocol)
 	commonMatchmakeExtensionProtocol := commonmatchmakeextension.NewCommonProtocol(matchmakeExtensionProtocol)
 	commonMatchmakeExtensionProtocol.SetManager(globals.MatchmakingManager)
-	//matchmakeExtensionProtocol.SetHandlerGetFriendNotificationData(getFriendNotificationData)
-	//matchmakeExtensionProtocol.SetHandlerUpdateNotificationData(updateNotificationData)
-	//matchmakeExtensionProtocol.GetPlayingSession = local_matchmakeextension.GetPlayingSession
 
 	messageDeliveryProtocol := messagedelivery.NewProtocol()
 	globals.SecureEndpoint.RegisterServiceProtocol(messageDeliveryProtocol)
 
 	messageDeliveryProtocol.DeliverMessage = local_message_delivery.DeliverMessage
 
-	commonMatchmakeExtensionProtocol.CleanupSearchMatchmakeSession = cleanupSearchMatchmakeSessionHandler
 	commonMatchmakeExtensionProtocol.CleanupMatchmakeSessionSearchCriterias = func(searchCriterias types.List[matchmakingtypes.MatchmakeSessionSearchCriteria]) {
 		for _, searchCriteria := range searchCriterias {
 			searchCriteria.Attribs[2] = types.NewString("")
